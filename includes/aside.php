12b671fa8b6b3986736ce5ecb498d6e79a7ad3f4
AkeemGit <500002961@my.fiveislands.uwi.edu>
2025-11-27 16:35:46 -0400

    Reusable components created and dashboard started

---
+++ includes/aside.php
@@ -0,0 +1,25 @@
+<aside class="sidebar">
+    <nav class="sidebar-menu">
+        <a href="home.php" class="menu-item">
+            <i class="fa-solid fa-house icon"></i>
+            <span class="text">Home</span>
+        </a>
+
+        <a href="new-contact.php" class="menu-item">
+            <i class="fa-solid fa-user-plus icon"></i>
+            <span class="text">New Contact</span>
+        </a>
+
+        <a href="users.php" class="menu-item">
+            <i class="fa-solid fa-users icon"></i>
+            <span class="text">Users</span>
+        </a>
+
+        <hr class="sidebar-divider">
+
+        <a href="logout.php" class="menu-item logout">
+            <i class="fa-solid fa-right-from-bracket icon"></i>
+            <span class="text">Logout</span>
+        </a>
+    </nav>
+</aside>
---
+++ pages/dashboard.php
@@ -0,0 +1,20 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="../assets/css/dashboard.css">
+    <link rel="stylesheet" href="../assets/css/aside.css">
+    <link rel="stylesheet" href="../assets/css/header.css">
+</head>
+
+<body>
+    <?php include "../includes/header.php"; ?>
+    <?php include "../includes/aside.php"; ?>
+
+</body>
+
+</html>

df7989d558f58d27cf586062a880eee29ad8df1e
Silford <silford.moore@my.fiveislands.uwi.edu>
2025-12-16 23:05:16 -0400

    Only admins can see Users tab on aside bar

--- includes/aside.php
+++ includes/aside.php
@@ -1,3 +1,22 @@
+<?php
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+
+$role = $_SESSION['role'] ?? null;
+
+if (!$role && isset($_SESSION['user_id'])) {
+    require '../config/db_connect.php';
+    $stmt = $pdo->prepare('SELECT role FROM users WHERE id = ?');
+    $stmt->execute([$_SESSION['user_id']]);
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($row && isset($row['role'])) {
+        $role = $row['role'];
+        $_SESSION['role'] = $role;
+    }
+}
+?>
+
 <aside class="sidebar">
     <nav class="sidebar-menu">
         <a href="../pages/dashboard.php" class="menu-item">
@@ -5,16 +24,18 @@
             <span class="text">Home</span>
         </a>
 
-        <a href="#" class="menu-item">
+        <a href="../pages/new-contact.php" class="menu-item">
             <i class="fa-solid fa-user-plus icon"></i>
             <span class="text">New Contact</span>
         </a>
 
-        <a href="#" class="menu-item">
-            <i class="fa-solid fa-users icon"></i>
-            <span class="text">Users</span>
-        </a>
-
+        <?php if ($role === 'admin'): ?> 
+            <a href="$" class="menu-item">
+                <i class="fa-solid fa-users icon"></i>
+                <span class="text">Users</span>
+            </a>
+        <?php endif; ?>
+        
         <hr class="sidebar-divider">
 
         <a href="../pages/logout.php" class="menu-item logout">
